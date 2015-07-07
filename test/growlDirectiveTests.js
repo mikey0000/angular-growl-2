afc545e8989722bb5830c3b36afeb6a7f0d66e10
Michael Arthur <michael.arthur@ikegps.com>
2015-07-08 10:29:35 +1200

    changing over to

--- test/growlDirectiveTests.js
+++ test/growlDirectiveTests.js
@@ -2,13 +2,15 @@ describe("growlDirective Spec", function() {
 	"use strict";
 
 	var $compile,
-		$rootScope;
+		$rootScope,
+		growl;
 
 	beforeEach(module('angular-growl'));
 
-	beforeEach(inject(function (_$compile_, _$rootScope_) {
+	beforeEach(inject(function (_$compile_, _$rootScope_, _growl_) {
 		$compile = _$compile_;
 		$rootScope = _$rootScope_;
+		growl = _growl_;
 	}));
 
 	it('Replaces the element with the appropriate content', function () {
@@ -18,5 +20,26 @@ describe("growlDirective Spec", function() {
 		$rootScope.$digest();
 
 		expect(growlElement.html()).toContain('<div class="growl-container growl-fixed top-right"');
+	});
+
+	it('should display and hide message', function (done) {
+
+		var growlElement = $compile('<div growl=""></div>')($rootScope);
+
+		$rootScope.$digest();
+
+		expect(growlElement.html()).toContain('<div class="growl-container growl-fixed top-right"');
+
+		growl.error('This is an error');
+
+		var growlScope = angular.element(growlElement).scope().$$childTail;
+
+		setTimeout(function() {
+			$rootScope.$digest();
+			expect(growlScope.growlMessages.getAllMessages().length).toEqual(0);
+			done();
+		}, 1100);
+
+
 	});
 });
--- test/growlMessageServiceTests.js
+++ test/growlMessageServiceTests.js
@@ -1,15 +1,20 @@
 describe("growlMessageService Spec", function() {
     "use strict";
 
-    var growlMessages;
+    var growlFactory,
+        growlMessages,
+        rootScope;
 
     beforeEach(module('angular-growl'));
-    beforeEach(inject(['growlMessages', function (gm) {
+    beforeEach(inject(['$rootScope', 'growl', 'growlMessages', function ($rootScope, gf, gm) {
+        growlFactory = gf;
         growlMessages = gm;
+        rootScope = $rootScope;
     }]));
 
     /// TESTS
     it('Should be defined', function () {
         expect(growlMessages).toBeDefined();
     });
+
 });
